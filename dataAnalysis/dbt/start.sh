5b857277e97ac052162bacdca747e4d19092beee
saurabhrai12 <58851193+saurabhrai12@users.noreply.github.com>
2024-10-17 10:56:44 +0530

    Update start.sh
     D   to check jenkins

--- dataAnalysis/dbt/start.sh
+++ dataAnalysis/dbt/start.sh
@@ -1,2 +1,2 @@
-dbt seed --profiles-dir /dbt/profiles --project-dir /dbt/DWH 
+dbt seed --profiles-dir /dbt/profiles --project-dir /Dbt/DWH 
 dbt run --profiles-dir /dbt/profiles --project-dir /dbt/DWH
